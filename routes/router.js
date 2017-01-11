73481ae518806c6bf370a35e283b2681b92f9bb2
rayzhang-xingyunzh <ray.zhang@xingyunzh.com>
2017-01-11 12:24:33 +0800

    remove test api

--- routes/router.js
+++ routes/router.js
@@ -25,8 +25,6 @@ module.exports = function(app, contextRoot) {
 
 	rootRouter.use('/api/user',userAPI);
     rootRouter.use('/api/image',imageAPI);
-
-    rootRouter.get('/api/wechat/ticket',cardController.getConfigParams);
     
     //id - user id
     //main method to render the card view
