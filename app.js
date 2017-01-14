9903c1c16b99b51b73d426c567ce43876af6cdeb
William Yu <williamyu>
2017-01-14 10:55:11 +0800

    s

--- app.js
+++ app.js
@@ -26,8 +26,6 @@ var	mongoURL = 'mongodb://' + envMongo.user +
 				':' + envMongo.port + 
 				'/' + envMongo.db;
 
-mongoURL = 'mongodb://localhost:27017/envelope'
-
 // create a new express server
 var app = express();
 
