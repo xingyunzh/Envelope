5bcffdf8ddac704171eaf62c7118dc2f09c40c71
rayzhang-xingyunzh <ray.zhang@xingyunzh.com>
2017-01-17 00:09:33 +0800

    bug fix

--- repositories/wechatRepository.js
+++ repositories/wechatRepository.js
@@ -5,16 +5,18 @@ var fs = require('fs');
 var stringHelper = require('../util/shared/stringHelper');
 var systemConfigRepository = require('./systemConfigRepository');
 
-var cacheFilePath = '/root/keys/cache.json';
+var cacheFilePath = '/root/cache/cache.json';
+
+var refreshTime = 2 * 60;
 
 exports.getConfigParams = function(url){
+
 	return getJSApiTicket().then(function(ticket){
 
 		return systemConfigRepository.getWechatCredentials().then(function(wechat){
-			console.log('wechat',wechat);
 			var appId = wechat.camproz.appId;
 			var nonceStr = stringHelper.randomString(10,'all');
-			var timeStamp = Math.floor(new Date() / 1000);
+			var timeStamp = getTimestamp();
 			var string1 = "jsapi_ticket=" + ticket + "&noncestr=" + nonceStr + "&timestamp=" + timeStamp + "&url=" + url;
 			var signature = generateSignature(string1);
 
@@ -31,95 +33,122 @@ exports.getConfigParams = function(url){
 };
 
 function getJSApiTicket(){
-	var deferred = q.defer();
 
-	var jsApiTicketCache = getFromCacheFile('jsApiTicket');
+	return getFromCacheFile('jsApiTicket').then(function(jsApiTicketCache){
 
-	if (!jsApiTicketCache.expiresIn || jsApiTicketCache.expiresIn < 60 * 2) {
-		getAccessToken().then(function(accessToken){
-			var url = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=" + accessToken + "&type=jsapi";
+		if (!jsApiTicketCache.expiresAt || jsApiTicketCache.expiresAt - getTimestamp() < refreshTime) {
 
-			https.get(url, function(res){
+			return getAccessToken().then(function(accessToken){
 
-				res.on('data', function(chunk){
-				    var resJSON = JSON.parse(chunk);
-				    if (resJSON.errcode == 0) {
-				    	storeToCacheFile('jsApiTicket',{
-				    		ticket:resJSON.ticket,
-				    		expires_in:resJSON.expires_in
-				    	});
+				var url = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=" + accessToken + "&type=jsapi";
 
-				   		deferred.resolve(resJSON.ticket);
-				    }else{
-				    	deferred.reject(resJSON.errmsg);
-				    }
-				    
+				var deferred = q.defer();
+
+				https.get(url, function(res){
+
+					res.on('data', function(chunk){
+					    var resJSON = JSON.parse(chunk);
+					    if (resJSON.errcode == 0) {
+					    	var promise = storeToCacheFile('jsApiTicket',{
+					    		ticket:resJSON.ticket,
+					    		expiresAt:getTimestamp() + resJSON.expires_in
+					    	}).then(function(){
+					    		return resJSON.ticket;
+					    	});
+
+					   		deferred.resolve(promise);
+					    }else{
+					    	deferred.reject(resJSON.errmsg);
+					    }
+					    
+					});
+				}).on('error', function(e){
+					console.error(e);
+					deferred.reject(e);
 				});
-			}).on('error', function(e){
-				console.error(e);
-				deferred.reject(e);
-			});
-		});
-		
-	}else{
-		deferred.resolve(jsApiTicketCache.jsApiTicket);
-	}
 
-	return deferred.promise;
+				return deferred.promise;
+			});
+			
+		}else{
+
+			return jsApiTicketCache.jsApiTicket;
+		}
+	});
+
+
 }
 
 function getAccessToken(){
-	var deferred = q.defer();
 
-	var accessTokenCache = getFromCacheFile('accessToken');
+	return getFromCacheFile('accessToken').then(function(accessTokenCache){
 
-	if (!accessTokenCache.expiresIn || accessTokenCache.expiresIn < 60 * 2) {
+		if (!accessTokenCache.expiresAt || accessTokenCache.expiresAt - getTimestamp() < refreshTime) {
 
-		var camproz = systemConfigRepository.getWechatCredentials().camproz;
-		var url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" + 
-					camproz.appId + 
-					"&secret=" + 
-					camproz.secret;
+			return systemConfigRepository.getWechatCredentials().then(function(wechat){
 
-		https.get(url,function(res){
+				var camproz = wechat.camproz;
 
-			res.on('data', function(chunk){
-			    var resJSON = JSON.parse(chunk);
-			    storeToCacheFile('accessToken',resJSON);
+				var url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" + 
+						camproz.appId + "&secret=" + camproz.secret;
 
-			    deferred.resolve(resJSON.access_token);
+				var deferred = q.defer();
+
+				https.get(url,function(res){
+
+					res.on('data', function(chunk){
+					    var resJSON = JSON.parse(chunk);
+
+					    var promise = storeToCacheFile('accessToken',{
+					    	accessToken:resJSON.access_token,
+					    	expiresAt:getTimestamp() + resJSON.expires_in
+					    }).then(function(){
+					    	return resJSON.accessToken;
+					    });
+
+					    deferred.resolve(promise);
+					    
+					});
+
+				}).on('error', function(e){
+					console.error(e);
+					deferred.reject(e);
+				});
+
+				return deferred.promise;
 			});
 
-		}).on('error', function(e){
-			console.error(e);
-			deferred.reject(e);
-		});
+		}else{
 
-	}else{
-		deferred.resolve(accessTokenCache.accessToken);
-	}
-
-	return deferred.promise;
+			return accessTokenCache.accessToken;
+		}
+	});
 }
 
 function storeToCacheFile(key,object){
-	var cache = JSON.parse(fs.readFileSync(cacheFilePath,'utf8'));
+	return q.nfcall(fs.readFile,cacheFilePath,'utf8').then(function(cacheString){
 
-	cache[key] = object;
+		var cache = JSON.parse(cacheString);
 
-	var cacheString = JSON.stringify(cache);
+		cache[key] = object;
 
-	fs.writeFileSync(cacheFilePath,cacheString,'utf8');
+		var newCacheString = JSON.stringify(cache);
+
+		return q.nfcall(fs.writeFile,cacheFilePath,newCacheString,'utf8');
+	});
 }
 
 function getFromCacheFile(key){
-	var cache = JSON.parse(fs.readFileSync(cacheFilePath,'utf8'));
 
-	if (key in cache) {
-		return cache[key];
-	}else{
-		return {};
-	}
+	return q.nfcall(fs.readFile,cacheFilePath,'utf8').then(function(cacheString){
+		var cache = JSON.parse(cacheString);
+
+		if (key in cache) {
+			return cache[key];
+		}else{
+			return {};
+		}
+	});
 }
 
 function generateSignature(string1){
@@ -129,6 +158,10 @@ function generateSignature(string1){
 	sha1.update(string1);
 
 	return sha1.digest('hex');
+}
+
+function getTimestamp(){
+	return Math.floor(new Date() / 1000);
 }
 
 
