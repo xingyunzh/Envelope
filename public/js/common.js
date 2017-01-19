59958fe50200c2d89279fda638d0d0a4ccd522a8
rayzhang-xingyunzh <ray.zhang@xingyunzh.com>
2017-01-19 16:09:37 +0800

    bug fix

--- public/js/common.js
+++ public/js/common.js
@@ -4,8 +4,10 @@ var cardCountOfLevel = [0,1,6,18,48,100];
 //var cardCountOfLevel = [1,2,3,4,5,6];
 
 function getLevelByCount(count){
-	
-	for(var level = 0;count > cardCountOfLevel[level + 1];level++);
+
+	for(var level = 0;count > cardCountOfLevel[level + 1];level++){
+		if (level >= 6) break;
+	}
 
 	return level;
 }
